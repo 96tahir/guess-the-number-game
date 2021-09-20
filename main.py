5fdfe6d5963ff6e422d14be782c049210bced836
Tahir Ahmed <88588946+96tahir@users.noreply.github.com>
2021-09-20 11:49:34 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+
+# generating random number from 1 to {num} numbers.
+num = 10
+randNumber = random.randrange(1, num)
+print(randNumber)
+
+# user input it will repeat repeat itself until the user guess the right number and track the number of tries of the user.
+userGuess = None
+guess = 0
+while userGuess != randNumber:
+    guess += 1
+    userGuess = int(input('Insert your guess: '))
+    if userGuess == randNumber:
+        print('Your have guessed it right!')
+    else:
+        if userGuess < randNumber:
+            print('Insert larger number!')
+        else:
+            print('insert smaller number!')
+
+print(f'you have gussed right numbers in {guess} tries!')
+
+# opening a file reading it if it has any previous high value.
+with open('hiscore.txt', 'r') as f:
+    hiscore = int(f.read())
+    
+    
+# storing new high value in text file and saving it.
+if guess < hiscore:
+    with open('hiscore.txt', 'w') as f:
+        f.write(str(guess))
